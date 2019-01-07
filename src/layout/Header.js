a12ac7c01f01cd6359ab6b68da56e27dc3c6db63
Enda Phelan <ephelan@redhat.com>
2019-01-07 16:35:19 +0000

    Created separate Header component

---
+++ src/layout/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { PageHeader, Brand, Avatar } from '@patternfly/react-core'
+import MyToolbar from './MyToolbar'
+import brandImg from './l_pf-reverse-164x11.png';
+import avatarImg from './img_avatar.svg';
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+        this.state = {
+            isNavOpen: props.isNavOpen
+        }
+    }
+
+    render() {
+        return (
+            <PageHeader
+                style={{ backgroundColor: '#cc0000' }}
+                logo={<Brand src={brandImg} alt="Patternfly Logo" />}
+                avatar={<Avatar src={avatarImg} alt="Avatar image" />}
+                showNavToggle
+                onNavToggle={this.props.onNavToggle}
+                toolbar={<MyToolbar />} />
+        )
+    }
+}
+
+export default Header
